6158e273ace4ff919235f690a7d79fdc3a609f0c
June Young Kim <systemblue.dev@proton.me>
2026-05-19 12:39:06 +0000

    Refactor slicearray and toSliceArray templates
    Refactor toSliceArray templates for better type handling and indexing. Introduce new methods for creating slicearray from various array types and improve indexing operations.

--- src/Utility/codeutils/arraytype.nim
+++ src/Utility/codeutils/arraytype.nim
@@ -1,50 +1,235 @@
 import std/macros
-import ../dataformat/dataformat
+import autoopt
+import envconst
+
+when Native and Unix:
+  import securetype
 
 type
+  # slicearray type declaration
+  slicearray*[len: static int, T] = distinct ptr UncheckedArray[T]
+
+#[
+  # legacy type
+  # this type is deprecated, pointer in object is much slower than pointer
   slicearray*[len: static int, T] = object
     data*: ptr UncheckedArray[T]
+]#
 
-proc toSliceArray*[N: static int, T](a: array[N, T], starts, ends: static int): auto {.inline.} =
-  static:
-    doAssert N > starts, "starts must smaller than N."
-    doAssert N > ends, "ends must smaller than N."
-    doAssert 0 <= starts, "starts must bigger than 0."
+# offset : calculate offset by ptr UncheckedArray[T]
+# internal template : not exported
+template offset[T](p: ptr UncheckedArray[T], i: int): ptr UncheckedArray[T] =
+  cast[ptr UncheckedArray[T]](cast[uint](p) + uint(i * sizeof(T)))
 
-  slicearray[ends - starts + 1, T](data: cast[ptr UncheckedArray[T]](addr a[starts]))
+# offset : calculate offset by slicearray[N, T]
+# internal template : not exported
+template offset[N: static int, T](p: slicearray[N, T], i: int): ptr UncheckedArray[T] =
+  cast[ptr UncheckedArray[T]](cast[uint](p) + uint(i * sizeof(T)))
 
-template `[]`*[L: static int, T](s: slicearray[L, T], i: static int): T =
-  static:
-    doAssert i >= 0 and i < L, "Index out of bounds (Getter)"
-  s.data[i]
+# toSliceArray : create new slicearray by array[N, T] and static int type starts, ends
+# starts : starts index, static int
+# ends : ends index, static int
+# compile time bound check
+# usage : when slicing array where the starts and ends are determined at compile time
+template toSliceArray*[N: static int, T](a: array[N, T], starts: static int, ends: static int): untyped =
+  static: doAssert starts >= 0 and ends < N and starts <= ends
+  cast[slicearray[ends - starts + 1, T]](addr a[starts])
 
-template `[] =`*[L: static int, T](s: slicearray[L, T], i: static int, value: T): void =
-  static:
-    doAssert i >= 0 and i < L, "Index out of bounds (Getter)"
-  s.data[i] = value
+# toSliceArray : create new slicearray by ptr array[N, T] and static int type starts, ends
+# starts : starts index, static int
+# ends : ends index, static int
+# compile time bound check
+# usage : when slicing ptr array where the starts and ends are determined at compile time
+template toSliceArray*[N: static int, T](a: ptr array[N, T], starts: static int, ends: static int): untyped =
+  static: doAssert starts >= 0 and ends < N and starts <= ends
+  cast[slicearray[ends - starts + 1, T]](addr a[starts])
 
-template `[]`*[L: static int, T](s: slicearray[L, T], i: int): T =
-  when not(defined(danger)):
-    doAssert i >= 0 and i < L, "Index out of bounds (Getter)"
-  s.data[i]
+# toSliceArray : create new slicearray by slicearray[N, T] and static int type starts, ends
+# starts : starts index, static int
+# ends : ends index, static int
+# compile time bound check
+# usage : when slicing slicearray where the starts and ends are determined at compile time
+template toSliceArray*[N: static int, T](a: slicearray[N, T], starts, ends: static int): untyped =
+  static: doAssert starts >= 0 and ends < N and starts <= ends
+  cast[slicearray[ends - starts + 1, T]](a.offset(starts))
 
-template `[] =`*[L: static int, T](s: slicearray[L, T], i: int, value: T): void =
-  when not(defined(danger)):
-    doAssert i >= 0 and i < L, "Index out of bounds (Getter)"
-  s.data[i] = value
+# toSliceArray : create new slicearray by openArray[T] and static int type starts, ends
+# starts : starts index, static int
+# ends : ends index, static int
+# run time bound check(deactivated in danger mode build)
+# usage : when slicing openArray where the starts and ends are determined at compile time
+template toSliceArray*[T](a: openArray[T], starts: static int, ends: static int): untyped =
+  when not defined(danger): doAssert a.len >= starts + ends - starts + 1
+  cast[slicearray[ends - starts + 1, T]](addr a[starts])
 
+# toSliceArray : create new slicearray by array[N, T] and int type starts, ends and static int type length
+# starts : starts index, int
+# ends : ends index, int
+# length : length, static int
+# compile time length check, run time bound check(deactivated in danger mode build)
+# usage : when creating a slicearray as an array in a loop (when the length is fixed)
+template toSliceArray*[N: static int, T](a: array[N, T], starts: int, ends: int, length: static int): untyped =
+  static: doAssert a.len >= length
+  when not defined(danger): doAssert a.len >= starts + length
+  cast[slicearray[length, T]](addr a[starts])
+
+# toSliceArray : create new slicearray by ptr array[N, T] and int type starts, ends and static int type length
+# starts : starts index, int
+# ends : ends index, int
+# length : length, static int
+# compile time length check, run time bound check(deactivated in danger mode build)
+# usage : when creating a slicearray as a ptr array in a loop (when the length is fixed)
+template toSliceArray*[N: static int, T](a: ptr array[N, T], starts: int, ends: int, length: static int): untyped =
+  static: doAssert a.len >= length
+  when not defined(danger): doAssert a.len >= starts + length
+  cast[slicearray[length, T]](addr a[starts])
+
+# toSliceArray : create new slicearray by slicearray[N, T] and int type starts, ends and static int type length
+# starts : starts index, int
+# ends : ends index, int
+# length : length, static int
+# compile time length check, run time bound check(deactivated in danger mode build)
+# usage : when creating a slicearray as a slicearray in a loop (when the length is fixed)
+template toSliceArray*[N: static int, T](a: slicearray[N, T], starts, ends: int, length: static int): untyped =
+  static: doAssert a.len >= length
+  when not defined(danger): doAssert a.len >= starts + length
+  cast[slicearray[length, T]](a.offset(starts))
+
+# toSliceArray : create new slicearray by openArray[T] and int type starts, ends and static int type length
+# starts : starts index, int
+# ends : ends index, int
+# length : length, static int
+# compile time length check, run time bound check(deactivated in danger mode build)
+# usage : when creating a slicearray as openArray in a loop (when the length is fixed)
+template toSliceArray*[T](a: openArray[T], starts: int, ends: int, length: static int): untyped =
+  when not defined(danger): doAssert a.len >= starts + length
+  cast[slicearray[length, T]](addr a[starts])
+
+# castTo : cast slicearray[N, T] to slicearray[sizeof(T) * N div sizeof(U), U]
+# usage : when passing a slicearray of a different type as an argument to a function that receives it
+proc castTo*[N: static int, T, U](s: slicearray[N, T], NewType: typedesc[U]): auto {.inline.} =
+  const oldSize = sizeof(T)
+  const newSize = sizeof(U)
+  const newLength = (N * oldSize) div newSize
+  static: doAssert (N * oldSize) mod newSize == 0
+  result = cast[slicearray[newLength, U]](s)
+
+# addr : get address of slicearray by index
+# index : int
+# run time bound check(deactivated in danger mode build)
+template `addr`*[L: static int, T](s: slicearray[L, T], index: int): pointer =
+  when not defined(danger): doAssert index >= 0 and index < L
+  addr s[index]
+
+# addr : get address of slicearray by index
+# index : static int
+# compile time bound check
+template `addr`*[L: static int, T](s: slicearray[L, T], index: static int): pointer =
+  static: doAssert index >= 0 and index < L
+  addr s[index]
+
+# read only indexing : get index and return value
+# index : static int
+# compile time bound check
+template `[]`*[L: static int, T](s: slicearray[L, T], index: static int): T =
+  static: doAssert index >= 0 and index < L
+  cast[ptr UncheckedArray[T]](s)[index]
+
+# mutable indexing : get index and set value
+# index : static int
+# compile time bound check
+template `[] =`*[L: static int, T](s: slicearray[L, T], index: static int, value: T) =
+  static: doAssert index >= 0 and index < L
+  cast[ptr UncheckedArray[T]](s)[index] = value
+
+# read only indexing : get index and return value
+# index : int
+# run time bound check(deactivated in danger mode build)
+template `[]`*[L: static int, T](s: slicearray[L, T], index: int): T =
+  when not defined(danger): doAssert index >= 0 and index < L
+  cast[ptr UncheckedArray[T]](s)[index]
+
+# mutable indexing : get index and set value
+# index : int
+# run time bound check(deactivated in danger mode build)
+template `[] =`*[L: static int, T](s: slicearray[L, T], index: int, value: T) =
+  when not defined(danger): doAssert index >= 0 and index < L
+  cast[ptr UncheckedArray[T]](s)[index] = value
+
+# items : get all items in slicearray
+# usage : when iterating over a slicearray
 iterator items*[L: static int, T](s: slicearray[L, T]): T =
   for i in static(0 ..< L):
-    yield s.data[i]
+    yield s[i]
 
-iterator mitems*[L: static int, T](s: var slicearray[L, T]): var T =
+# mitems : get all items in slicearray
+# usage : when iterating over a slicearray
+iterator mitems*[L: static int, T](s: slicearray[L, T]): var T =
   for i in static(0 ..< L):
-    yield s.data[i]
+    yield s[i]
 
+# toString opeartor : convert slicearray to string
+# usage : when printing a slicearray
 template `$`*[L: static int, T](s: slicearray[L, T]): string =
   var output: string = "["
   for i in static(0 ..< L):
-    output.add $s.data[i]
+    output.add $s[i]
     if i < L - 1:
       output.add ", "
     output.add "]"
+#[
+# static indexing : get index and return static value
+# index : static int
+# return : static T
+# usage : when accessing a static(const) array in static(const) index
+template `[]`*[N, T](arr: static array[N, T], index: static int): static T =
+  arr[index]
+]#
+# backreference operator : slicearray to slicearry
+template `[]`*[L: static int, T](s: slicearray[L, T]): slicearray[L, T] =
+  s
+
+# toArray : convert slicearray to array
+template toArray*[L: static int, T](s: slicearray[L, T]): array[L, T] =
+  var output: array[L, T]
+  # use copyMem when not in js, nimvm, pipelineC
+  when not(defined(js) and defined(nimvm) and defined(pipelinec)):
+    copyMem(addr output[0], addr s[0], sizeof(T) * L)
+  # else use unroll to copy elements
+  else:
+    unroll(i, 0, L - 1):
+      output[i] = s[i]
+  output
+
+# backreference copy : copy slicearray to slicearray
+# usage : when copying a slicearray to a slicearray
+proc `[] =`*[N: static int, T](output: slicearray[N, T], input: slicearray[N, T]) {.inline.} =
+  # use copyMem when not in js, nimvm, pipelineC
+  when not(defined(js) and defined(nimvm) and defined(pipelinec)):
+    copyMem(addr output[0], addr input[0], N * sizeof(T))
+  # else use unroll to copy elements
+  else:
+    unroll(i, 0, N - 1):
+      output[i] = input[i]
+
+# backreference copy : copy array to slicearray
+# usage : when copying an array to a slicearray
+proc `[] =`*[N: static int, T](output: slicearray[N, T], input: array[N, T]) {.inline.} =
+  # use copyMem when not in js, nimvm, pipelineC
+  when not(defined(js) and defined(nimvm) and defined(pipelinec)):
+    copyMem(addr output[0], addr input[0], N * sizeof(T))
+  # else use unroll to copy elements
+  else:
+    unroll(i, 0, N - 1):
+      output[i] = input[i]
+
+# backreference copy : copy ptr array to slicearray
+# usage : when copying a ptr array to a slicearray
+proc `[] =`*[N: static int, T](output: slicearray[N, T], input: ptr array[N, T]) {.inline.} =
+  # use copyMem when not in js, nimvm, pipelineC
+  when not(defined(js) and defined(nimvm) and defined(pipelinec)):
+    copyMem(addr output[0], addr input[0], N * sizeof(T))
+  # else use unroll to copy elements
+  else:
+    unroll(i, 0, N - 1):
+      output[i] = input[i]
